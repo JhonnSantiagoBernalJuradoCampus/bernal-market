5663db62a76d1c1f7de9f04b5f2f9bf5be6bbbbe
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2024-05-12 13:16:20 -0500

    Hola mundo en Spring

---
+++ src/main/java/com/bernimarket/BerniMarketApplication.java
@@ -0,0 +1,13 @@
+package com.bernimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerniMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerniMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernimarket/BerniMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerniMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
