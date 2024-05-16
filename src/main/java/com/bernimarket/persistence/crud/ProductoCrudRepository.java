b6182f128bcf5a9626493a99cab6c50376051b18
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2024-05-16 01:34:51 -0500

    Podemos hacer un query de manera nativa con esta notacion

--- src/main/java/com/bernimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/bernimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,8 @@ package com.bernimarket.persistence.crud;
 import com.bernimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoria(int idCategoria);
 }
