8d72547681bbdeff08376eb0436f12e846c81ad7
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2024-05-16 01:52:49 -0500

    Repository listo para implementarse

--- src/main/java/com/bernimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/bernimarket/persistence/crud/ProductoCrudRepository.java
@@ -4,7 +4,10 @@ import com.bernimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
-    List<Producto> findByIdCategoria(int idCategoria);
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
